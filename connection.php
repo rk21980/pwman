580832e19a6205f7570f7d39f2ae3c5c711fd2ce
Rob <rk21980@gmail.com>
2016-09-27 21:02:30 +0100

    MVC initial setup commits

--- connection.php
+++ connection.php
@@ -19,8 +19,8 @@ class Db
         if( !isset(self::$instance)) {
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
             try {
-                //self::$instance = new PDO('mysql:host=127.0.0.1;dbname=robkantor1', 'pwmanager@localhost', 'Ch33s3cak3', $pdo_options);
-                self::$instance = mysqli_connect( "217.174.253.159", "pwmanuser", 'Ch33s3cak3', "robkantor1" );
+                self::$instance = new PDO('mysql:host=217.174.253.159;dbname=robkantor1', 'pwmanuser', 'Ch33s3cak3', $pdo_options);
+                //self::$instance = mysqli_connect( "217.174.253.159", "pwmanuser", 'Ch33s3cak3', "robkantor1" );
             } catch (PDOException $e) {
                 echo "Connection failed: " . $e->getMessage();
             }
