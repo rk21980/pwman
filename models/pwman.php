5033487eb6c2e97e530a72fb19b6091091fd000a
Rob <rk21980@gmail.com>
2016-09-28 20:38:59 +0100

    MVC initial setup commits

--- models/pwman.php
+++ models/pwman.php
@@ -15,6 +15,9 @@ class PWMan_Ground
     public function __construct()
     {
         $db = Db::getInstance();
+
+        var_dump(Db::hasTable(self::PIECE_TABLE));
+
         if(!Db::hasTable(self::PIECE_TABLE)) {
             $_statement = $db->prepare("CREATE TABLE :table 
                                       piece_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
