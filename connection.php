c606b69d0c87cfdc331ebfc041af935da1ac03e5
Rob <rk21980@gmail.com>
2016-09-28 20:37:20 +0100

    MVC initial setup commits

--- connection.php
+++ connection.php
@@ -16,8 +16,9 @@ class Db
 
     private function __clone() {}
 
-    public static function getInstance() {
-        if( !isset(self::$_instance)) {
+    public static function getInstance()
+    {
+        if (!isset(self::$_instance)) {
             $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
             try {
                 self::$_instance = new PDO('mysql:host=217.174.253.159;dbname=robkantor1', 'pwmanuser', 'Ch33s3cak3', $pdo_options);
@@ -30,17 +31,19 @@ class Db
 
     public static function hasTable( $tableName = NULL )
     {
-        echo "table: ".$tableName;
-        if( !empty($tableName) ) {
+        echo "table: " . $tableName;
+        if (!empty($tableName)) {
             if (empty(self::$_tables)) {
                 $db = self::getInstance();
                 $_tables = $db->prepare("SHOW TABLES LIKE ':table'");
                 $_tables->execute(array(":table" => $tableName));
                 self::$_tables = $_tables;
+                $_tables->errorInfo();
                 $_tables->debugDumpParams();
             }
-            echo "CC: ".self::$_tables->columnCount();
-            if(self::$_tables->columnCount())
+            echo "CC: " . self::$_tables->columnCount();
+            self::$_tables->fetchAll();
+            if (self::$_tables->columnCount())
                 return true;
         }
         return false;
